c9f1d89f77776f4ebb547a229cd4197928bff155
Serdiuk-Maksym <serdyukmaksimaleksandrovich@gmail.com>
2024-02-12 03:14:15 +0100

    some changes

--- app.js
+++ app.js
@@ -1,26 +1,18 @@
-import express from "express";
-import morgan from "morgan";
-import cors from "cors";
+import mongoose from 'mongoose';
 
-import contactsRouter from "./routes/contactsRouter.js";
+// Підключення до MongoDB за допомогою Mongoose
+mongoose.connect(
+  'mongodb+srv://serdiukMO:Vfrc1992fcec@cluster0.z162u5v.mongodb.net/',
+  {}
+);
 
-const app = express();
+const db = mongoose.connection;
 
-app.use(morgan("tiny"));
-app.use(cors());
-app.use(express.json());
-
-app.use("/api/contacts", contactsRouter);
-
-app.use((_, res) => {
-  res.status(404).json({ message: "Route not found" });
+db.on('error', (error) => {
+  console.error('Database connection failed:', error);
+  process.exit(1);
 });
 
-app.use((err, req, res, next) => {
-  const { status = 500, message = "Server error" } = err;
-  res.status(status).json({ message });
-});
-
-app.listen(3000, () => {
-  console.log("Server is running. Use our API on port: 3000");
+db.once('open', () => {
+  console.log('Database connection successful');
 });
