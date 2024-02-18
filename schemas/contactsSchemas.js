ee1c1928a33cc82522b78924bbb2cabdf3f913b7
Serdiuk-Maksym <serdyukmaksimaleksandrovich@gmail.com>
2024-02-18 03:45:21 +0100

    some fix

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,18 +1,19 @@
-import mongoose from 'mongoose';
+import Joi from 'joi';
 
-const contactSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Set name for contact'],
-  },
-  email: String,
-  phone: String,
-  favorite: {
-    type: Boolean,
-    default: false,
-  },
+export const createContactSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string().required(),
+  favorite: Joi.boolean(),
 });
 
-const Contact = mongoose.model('Contact', contactSchema);
+export const updateContactSchema = Joi.object({
+  name: Joi.string(),
+  email: Joi.string().email(),
+  phone: Joi.string(),
+  favorite: Joi.boolean(),
+}).or('name', 'email', 'phone', 'favorite');
 
-export default Contact;
+export const updateFavoriteSchema = Joi.object({
+  favorite: Joi.boolean().required(),
+});
